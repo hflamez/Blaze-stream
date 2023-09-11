6640f3bc8bb926ed8e447b64bf8da63412458ad7
Flamez <helxie3@gmail.com>
2023-09-11 14:53:17 +0100

    monday

--- lib/pages/input_loginPage.dart
+++ lib/pages/input_loginPage.dart
@@ -1,7 +1,11 @@
+
+
+import 'package:blaze_stream/buttons/primaryButtons.dart';
 import 'package:blaze_stream/pages/background.dart';
 import 'package:blaze_stream/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 
 class InputLoginPage extends StatefulWidget {
   const InputLoginPage({Key? key, String title = ''}) : super(key: key);
@@ -11,6 +15,8 @@ class InputLoginPage extends StatefulWidget {
 }
 
 class InputLoginPageState extends State<InputLoginPage> {
+  bool showAnimated = false;
+  bool showPass = false;
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
@@ -23,6 +29,8 @@ class InputLoginPageState extends State<InputLoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -49,58 +57,96 @@ class InputLoginPageState extends State<InputLoginPage> {
       body: Stack(
         children: [
           const Back_Ground(),
+
           SizedBox(
            
             child: Column(
              mainAxisAlignment: MainAxisAlignment.center,
              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+              const  SizedBox(
+                  
+                
+                ),
+                Text('Hi, Hope you are having a nice day?',
+                style: GoogleFonts.nunito(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700
+                     ), ),
+                        Text('Welcome Back! please enter your details ',
+                style: GoogleFonts.nunito(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700
+                     ), ),
                 Padding(
                   padding: const EdgeInsets.all(10),
                   child: TextField(
                     controller: _email,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'User Name',
+                      labelText: 'Gmail',
                       hintText: 'Enter valid mail id as abc@gmail.com',
                     ),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(10),
+                  padding:const  EdgeInsets.all(10),
                   child: TextField(
                     controller: _password,
-                    obscureText: true,
+                    obscureText: !showPass,
                     enableSuggestions: false,
                     autocorrect: false,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
+                    decoration:  InputDecoration(
+                      border: const OutlineInputBorder(),
                       labelText: 'Password',
                       hintText: 'Enter your secure password',
+    suffixIcon: IconButton(
+      onPressed: () {
+        setState( () {
+          showPass = !showPass; // Toggle the password visibility
+        });
+      },
+      icon: Icon(
+        showPass ?Icons.visibility : Icons.visibility_off,
+        color: Colors.grey,
+      ),),
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, loginRoute);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+               TextButton(onPressed: (){}
+                 , child:Text("Forgot Pasword?",
+                 style: GoogleFonts.nunito(
+                  color: Colors.grey,
+                 textStyle: const TextStyle(
+                 decoration: 
+                   TextDecoration.underline
                     ),
-                  ),
-                  child: const Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+            ),)),
+
+             const SizedBox( height: 20,
             ),
+            GestureDetector(
+              onTap: () {
+                
+              },
+              child: PrimaryButton(
+                title:  "Login",
+                width: screenWidth * .8,
+                height: 50,
+                
+              ),
+            )
+              ],
+              
+            ),
+            
           ),
+       
+        
         ],
+        
       ),
     );
   }
